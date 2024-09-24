4603489b70b0cd04efe5d777d756ffc55efb9cb4
Kavish Ambani <kavishkkambani@gmail.com>
2024-09-24 20:49:34 +0530

    fixing error handling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,19 @@ import { VideoCard } from '../components/VideoCard';
 import axios from 'axios';
 function Home() {
     const [video, setVideo] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
     useEffect(() => {
         const videos = async () => {
             try {
                 const video = await axios.get("https://vtapi.kavishambani.in/api/v1/video/");
                 setVideo(video.data.data.docs);
+                setLoading(false)
                 document.title = "VideoTube";
             } catch (error) {
+                setError("Error in loading videos")
                 console.error("error in loading videos", error);
+                setLoading(false)
             }
         }
         videos();
@@ -23,22 +28,29 @@ function Home() {
             </div>
             <div className='bg-black min-h-screen text-white p-4'>
                 <div className='flex flex-wrap justify-center gap-4'>
-                    {video[0]?._id ? video.map(video => (
-                        <VideoCard
-                            key={video._id}
-                            thumbUrl={video.thumbnail}
-                            title={video.title}
-                            views={video.views}
-                            owner={video.ownerDetails.username}
-                            videoId={video._id}
-                            createdAt={video.createdAt}
-                            avatar={video.ownerDetails.avatar}
-                        />
-                    )) : "Error in Loading videos"}
+                {loading ? (
+                        <div>Loading videos...</div>
+                    ) : error ? (
+                        <div>{error}</div>
+                    ) : video.length > 0 ? (
+                        video.map(video => (
+                            <VideoCard
+                                key={video._id}
+                                thumbUrl={video.thumbnail}
+                                title={video.title}
+                                views={video.views}
+                                owner={video.ownerDetails.username}
+                                videoId={video._id}
+                                createdAt={video.createdAt}
+                                avatar={video.ownerDetails.avatar}
+                            />
+                        ))
+                    ) : (
+                        <div>No videos found</div>
+                    )}
                 </div>
             </div>
         </>
     )
 }
-
 export default Home
