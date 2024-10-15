ce1d65215ac99aa79d06a4cdbcbad91f46acf33d
Kavish Ambani <kavishkkambani@gmail.com>
2024-10-15 20:30:34 +0530

    test navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -81,11 +81,13 @@ export const NavBar = () => {
                             <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
                         </svg>
                     </div>
-                    <div className='relative' ref={desktopProfRef}>
+                    {auth?.user ? (
+                        <div className='relative' ref={desktopProfRef}>
                         <div className="rounded-full h-12 w-12 hover:text-x cursor-pointer bg-black border-2 text-white flex justify-center drop-shadow-lg shadow-white mr-2" onClick={toggleDesktopProf}>
-                            <div className="flex flex-col justify-center h-full text-xl">
-                                K
-                            </div>
+                            {auth?.user?.avatar ? (<img src={auth.user.avatar} />) : (
+                                <div className="flex flex-col justify-center h-full text-xl">
+                                    {auth?.user?.username?.charAt(0).toUpperCase() || 'K'}
+                                </div>)}
                         </div>
                         {desktopProfDrop && (
                             <div className='absolute right-0 mt-2 w-48 bg-black text-white rounded-lg shadow-md shadow-x py-2 z-50'>
@@ -100,6 +102,9 @@ export const NavBar = () => {
                             </div>
                         )}
                     </div>
+                    ) : <button>
+                            l
+                        </button>}
                 </div>
                 {isOpen && (
                     <div className="md:hidden absolute top-12 left-0 w-[90%] bg-black rounded-lg shadow-md shadow-x p-8 z-50">
@@ -121,9 +126,9 @@ export const NavBar = () => {
                                     <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
                                 </svg>
                             </div>
-                            {auth ? <div className='relative' ref={mobileProfRef}>
+                            {auth?.user ? <div className='relative' ref={mobileProfRef}>
                                 <div className="rounded-full h-12 w-12 hover:text-x cursor-pointer bg-black border-2 text-white flex justify-center drop-shadow-lg shadow-white" onClick={toggleMobileProf}>
-                                   <img src={auth?.avatar}/>
+                                    <img src={auth.user.avatar} />
                                 </div>
                                 {mobileProfDrop && (
                                     <div className='absolute right-0 mt-2 w-48 bg-black text-white rounded-lg shadow-md shadow-x py-2 z-50'>
@@ -138,9 +143,9 @@ export const NavBar = () => {
                                     </div>
                                 )}
                             </div> : (
-                                <div className='text-white'>
-                                    k
-                                </div>
+                                <button>
+                                    l   
+                                </button>
                             )}
                         </div>
                     </div>
