6eb8c42c58ef1e16b3e173919c59127dec5df1ac
Kavish Ambani <kavishkkambani@gmail.com>
2025-01-12 00:28:49 +0530

    change placeholder color

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -10,7 +10,7 @@ export const Input = ({
             <div className="block text-sm font-medium text-left py-2 mb-1">
                 {label}
             </div>
-            <input onChange={onChange} placeholder={placeholder} value={value} type={type} className="w-full px-4 py-2 bg-black text-white border border-red-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-transparent placeholder-lightGray transition-colors duration-200" />
+            <input onChange={onChange} placeholder={placeholder} value={value} type={type} className="w-full px-4 py-2 bg-black text-white border border-red-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-transparent placeholder-darkGray transition-colors duration-200" />
         </div>
     )
 }
