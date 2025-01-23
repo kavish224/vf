7d757dda765905e1d9e8143f8917f082bb265fd7
Kavish Ambani <kavishkkambani@gmail.com>
2025-01-23 15:01:32 +0530

    improve styling

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -6,11 +6,11 @@ export const Input = ({
     type = "text"
 }) => {
     return(
-        <div className="w-full mb-4">
-            <div className="block text-sm font-medium text-left py-2 mb-1">
+        <div className="w-full">
+            <div className="block text-sm font-medium text-left">
                 {label}
             </div>
-            <input onChange={onChange} placeholder={placeholder} value={value} type={type} className="w-full px-4 py-2 bg-black text-white border border-red-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-transparent placeholder-darkGray transition-colors duration-200" />
+            <input onChange={onChange} placeholder={placeholder} value={value} type={type} className="w-full px-4 py-2 bg-black text-white border border-x rounded-lg focus:outline-none focus:ring-2 focus:ring-white focus:border-transparent placeholder-darkGray transition-colors duration-200" />
         </div>
     )
 }
